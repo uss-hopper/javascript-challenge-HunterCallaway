d7d73245f63dd4c8f6d16937c28ad6bf5ad34275
Hunter Callaway <HunterCallaway@hotmail.com>
2019-02-18 18:47:06 -0700

    Started writing the first JavaScript function.

---
+++ script.js
@@ -0,0 +1,9 @@
+//Highlight a word
+function highlightWord(text) {
+	const emily = document.getElementById("emily");
+	const innerHtml = emily.innerHTML;
+	let index = innerHtml.indexOf(text);
+	if (index >= 0) {
+		
+	}
+}
